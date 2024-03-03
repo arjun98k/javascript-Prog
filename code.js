36cb52f34af75e94bfa2e8c0d42b432d5bc213f7
arjun98k <arjunkandekar14@gmail.com>
2024-03-03 13:04:01 +0530

    add the file to github

---
+++ code.js
@@ -0,0 +1,16 @@
+let num1 = 12
+let num2 = 10
+num1 = 1 + num1
+let age = 18
+
+if (age == 18) {
+    console.log('you can drive');
+} else {
+    console.log("you cannot drive");
+}
+
+// console.log(num1+num2)
+// console.log(num1%num2)
+// console.log(num1/num2)
+// console.log(++num1);
+// console.log(num1);
