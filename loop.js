f56ca6dfaf637adce6cac8b3cc444992eabc903f
arjun98k <arjunkandekar14@gmail.com>
2024-03-04 01:03:36 +0530

    add file

---
+++ loop.js
@@ -0,0 +1,19 @@
+
+
+// for Loop
+
+let b = 1
+for (b ; b < 11; b++) {
+    var c = b
+    
+}
+console.log(c);
+
+// while loop
+
+let a = 1
+while (a<11) {
+    var c = a;
+    a++;
+}
+console.log(c);
